a09b05a8bc92cbc779ee5872924ac04112611305
Alexey Yuhin <Alexey.Yuhin@mir.dev>
2021-12-18 20:55:57 +0000

    init commit

---
+++ inc/AsyncFunctor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <functional>
+#include "ITask.hpp"
+#include "freertos/task.h"
+
+struct AsyncFunctor : public ITask{
+    using function_t = std::function<void()>;
+
+    explicit AsyncFunctor(function_t function, const std::string& taskName = "Task", const uint16_t stackSize = configMINIMAL_STACK_SIZE, const uint8_t priority = defaultPriority)
+        :   ITask(taskName, stackSize, priority),
+            _func{function} {
+
+    }
+
+    void run(void* args) override {
+        if (_func) {
+            _func();
+        }
+    }
+    private:
+        function_t _func = nullptr;
+};
